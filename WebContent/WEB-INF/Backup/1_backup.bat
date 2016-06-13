c49de586afc4b886f358db7ad6c87d889a23fd88
Fabricio Teixeira Nascimento <fabriciotn@yahoo.com.br>
2016-06-13 14:58:31 -0300

    Criando a pasta caso não exista.

--- WebContent/WEB-INF/Backup/1_backup.bat
+++ WebContent/WEB-INF/Backup/1_backup.bat
@@ -1,6 +1,9 @@
+IF NOT EXIST "C:\TTFAuditorBackup" (
+	md "C:\TTFAuditorBackup"	
+)
 cd\
 cd "C:\Program Files\MySQL\MySQL Server 5.6\bin"
-mysqldump --add-drop-database --add-drop-table -h 192.168.25.5 --port 3306 -u root --password=root -B auditoria_db -r "C:\TTFAuditorBackup\backup.sql"
+mysqldump --add-drop-database --add-drop-table -h 10.20.8.209 --port 3306 -u root --password=root -B auditoria_db -r "C:\TTFAuditorBackup\backup.sql"
 
 net stop MySQL56
 net start MySQL56
